f23bae6d794b389b45329ddca9944be46af8a948
blacklaws <kdkmbusiness@gmail.com>
2019-12-17 20:38:44 -0500

    Day 8 Part 2

--- Day_8/Day_8.py
+++ Day_8/Day_8.py
@@ -3,9 +3,34 @@ with open('Day_8\input.txt') as f:
     image_data = f.read().strip()
 h = 6
 w = 25
+# image_data = '0222112222120000'
+# h = 2
+# w = 2
 
-layers = [(image_data[s:s+h*w]) for s in range(0,len(image_data),h*w)]
+layers = [image_data[s:s+h*w] for s in range(0,len(image_data),h*w)]
 
-layers_digit_counts = sorted((Counter(layer) for layer in layers), key = lambda x: x['0'])
-print(layers_digit_counts)
-print (layers_digit_counts[0]['1']*layers_digit_counts[0]['2'])
+# layers_digit_counts = sorted((Counter(layer) for layer in layers), key = lambda x: x['0'])
+# print(layers_digit_counts)
+# print (layers_digit_counts[0]['1']*layers_digit_counts[0]['2'])
+
+
+all_pixels =  zip(*layers)
+pixels = [''.join(p) for p in all_pixels]
+
+image = ''
+for pixel in pixels:
+    
+    for layer_color in pixel:
+        if layer_color !='2':
+            if layer_color == '0':
+                image += " "
+            else: 
+                image += '#'
+            break
+            
+
+for line in range(0,len(image),w):
+    print(image[line:line+w])
+        
+
+
